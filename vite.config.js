db2a70fc78c246a1c9de99f5c396261999f433c7
Leo <leo.hilsheimer@gmail.com>
2026-09-02 15:24:03 +0200

    feat: PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(() => {
   return {
@@ -9,7 +10,37 @@ export default defineConfig(() => {
     },
     plugins: [
       react(),
+      // support old browsers
       legacy(),
+      // make it a PWA
+      VitePWA({
+        registerType: "autoUpdate",
+        manifestFilename: "site.webmanifest",
+        includeAssets: ["sounds/*.{mp3,wav}"],
+        manifest: {
+          id: "/",
+          name: "Steps Sampler",
+          short_name: "Steps Sampler",
+          description: "Create beats with a simple step sequencer and sampler.",
+          start_url: "/",
+          scope: "/",
+          display: "standalone",
+          theme_color: "#fefcf3",
+          background_color: "#fefcf3",
+          icons: [
+            {
+              src: "/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
     ],
     server: {
       allowedHosts: ["localhost", "steps.leohilsheimer.com"],
